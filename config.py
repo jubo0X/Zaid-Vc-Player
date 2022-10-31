cbddb620ff6c1fbd539aec9534e1c1da959f9642
jubo000 <30005726+jubo0X@users.noreply.github.com>
2022-10-31 15:09:44 +0600

    Update config.py

--- config.py
+++ config.py
@@ -13,22 +13,22 @@ admins = {}
 
 SESSION_NAME = getenv("SESSION_NAME")
 
-if str(getenv("STRING_SESSION2")).strip() == "":
+if str(getenv("STRING_SESSION2")).strip() == "AQAxvsvCS1gUoRQaRXN4wk0RcAz5mI_X6W3CjI_z0zZGzAfV9li-bqmhypOCT69nQ0F2xvF9_il9UaAvSrT5GMB1ldF61fvUOoV3fwjhOuXyOEqwZh79xDSsWrfx3eol6xXApOUH81nYAyBeVSLbyrHxV4S3q9rArcdN8c9_-DEaL4POCpe5duyHXBrhCSJ8sADWff__Bu54s5NwVtXLbDHu5X7VtYvCGj5uBzsZ8lM8rv-ZxSDoImac6cV1O8IgLsfZ13xrcPpftPJWwRQKZ1MwSYUeLQ3DTuA8CesOyz5R32QfReXdffy_nQnTk2nATyOH8B6D5z69QfqNl04EXcM-AAAAAVDdpxEA":
     SESSION2 = str(None)
 else:
     SESSION2 = str(getenv("STRING_SESSION2"))
 
-if str(getenv("STRING_SESSION3")).strip() == "":
+if str(getenv("STRING_SESSION3")).strip() == "AQAxvsvCS1gUoRQaRXN4wk0RcAz5mI_X6W3CjI_z0zZGzAfV9li-bqmhypOCT69nQ0F2xvF9_il9UaAvSrT5GMB1ldF61fvUOoV3fwjhOuXyOEqwZh79xDSsWrfx3eol6xXApOUH81nYAyBeVSLbyrHxV4S3q9rArcdN8c9_-DEaL4POCpe5duyHXBrhCSJ8sADWff__Bu54s5NwVtXLbDHu5X7VtYvCGj5uBzsZ8lM8rv-ZxSDoImac6cV1O8IgLsfZ13xrcPpftPJWwRQKZ1MwSYUeLQ3DTuA8CesOyz5R32QfReXdffy_nQnTk2nATyOH8B6D5z69QfqNl04EXcM-AAAAAVDdpxEA":
     SESSION3 = str(None)
 else:
     SESSION3 = str(getenv("STRING_SESSION3"))
 
-if str(getenv("STRING_SESSION4")).strip() == "":
+if str(getenv("STRING_SESSION4")).strip() == "AQAxvsvCS1gUoRQaRXN4wk0RcAz5mI_X6W3CjI_z0zZGzAfV9li-bqmhypOCT69nQ0F2xvF9_il9UaAvSrT5GMB1ldF61fvUOoV3fwjhOuXyOEqwZh79xDSsWrfx3eol6xXApOUH81nYAyBeVSLbyrHxV4S3q9rArcdN8c9_-DEaL4POCpe5duyHXBrhCSJ8sADWff__Bu54s5NwVtXLbDHu5X7VtYvCGj5uBzsZ8lM8rv-ZxSDoImac6cV1O8IgLsfZ13xrcPpftPJWwRQKZ1MwSYUeLQ3DTuA8CesOyz5R32QfReXdffy_nQnTk2nATyOH8B6D5z69QfqNl04EXcM-AAAAAVDdpxEA":
     SESSION4 = str(None)
 else:
     SESSION4 = str(getenv("STRING_SESSION4"))
 
-if str(getenv("STRING_SESSION5")).strip() == "":
+if str(getenv("STRING_SESSION5")).strip() == "AQAxvsvCS1gUoRQaRXN4wk0RcAz5mI_X6W3CjI_z0zZGzAfV9li-bqmhypOCT69nQ0F2xvF9_il9UaAvSrT5GMB1ldF61fvUOoV3fwjhOuXyOEqwZh79xDSsWrfx3eol6xXApOUH81nYAyBeVSLbyrHxV4S3q9rArcdN8c9_-DEaL4POCpe5duyHXBrhCSJ8sADWff__Bu54s5NwVtXLbDHu5X7VtYvCGj5uBzsZ8lM8rv-ZxSDoImac6cV1O8IgLsfZ13xrcPpftPJWwRQKZ1MwSYUeLQ3DTuA8CesOyz5R32QfReXdffy_nQnTk2nATyOH8B6D5z69QfqNl04EXcM-AAAAAVDdpxEA":
     SESSION5 = str(None)
 else:
     SESSION5 = str(getenv("STRING_SESSION5"))
